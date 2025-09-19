30011437105e763ef5a50c014f86d4df8bf7c1d8
Zack <112123433+Shamr0ckz@users.noreply.github.com>
2025-09-19 11:46:25 -0700

    Create EXO-ShareCalendar.ps1

---
+++ M365/ExchangeOnline/EXO-ShareCalendar.ps1
@@ -0,0 +1,22 @@
+#Set-ExecutionPolicy RemoteSigned -Force -Confirm:$false;
+
+#Check for Exchannge Online module and install if missing#
+if(-not (Get-Module ExchangeOnlineManagement -ListAvailable)){
+Write-Host "Installing Exchange Online module for current user"
+Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force
+} else {
+	write-host "Exchange Online module already installed"
+}
+
+#Connect to Exchange Online (EXO)#
+$AdminUPN = Read-Host "Enter your Exchange Online admin UPN"
+Connect-ExchangeOnline -UserPrincipalName $AdminUpn
+
+#Set variables#
+$CalendarUPN = Read-Host "Enter UPN of user's calendar to share"
+$CalendarPath = $CalendarUPN + ":\Calendar"
+$CalendarDelegate = Read-Host "Enter UPN of delegate to share the calendar with"
+$Permissions = Read-Host "Enter permissions to grant" $CalendarDelegate "(Owner, Editor, Reviewer, etc.)"
+
+#Execute share#
+Add-MailboxFolderPermission -Identity $CalendarUPN -User $CalendarDelegate -AccessRights Editor
